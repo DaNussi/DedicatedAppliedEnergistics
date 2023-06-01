169ce8ad6b4e818348765574b321d6d5fd1a7f88
DaNussi <daniel.nussbaum@htlstp.at>
2023-06-01 19:41:46 +0200

    Everything is fucked :C

--- src/main/java/net/nussi/dedicated_applied_energistics/blockentities/InterDimensionalInterfaceBlockEntity.java
+++ src/main/java/net/nussi/dedicated_applied_energistics/blockentities/InterDimensionalInterfaceBlockEntity.java
@@ -1,55 +1,128 @@
 package net.nussi.dedicated_applied_energistics.blockentities;
 
+import appeng.api.inventories.BaseInternalInventory;
+import appeng.api.inventories.InternalInventory;
+import appeng.blockentity.AEBaseInvBlockEntity;
 import com.mojang.logging.LogUtils;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.nbt.TagParser;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.ForgeCapabilities;
-import net.minecraftforge.common.util.LazyOptional;
-import net.nussi.dedicated_applied_energistics.capabilities.InterDimensionalInterfaceCapability;
 import net.nussi.dedicated_applied_energistics.init.BlockEntityTypeInit;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
-public class InterDimensionalInterfaceBlockEntity extends BlockEntity {
-    private final InterDimensionalInterfaceCapability itemHandler = new InterDimensionalInterfaceCapability();
-    private final LazyOptional<InterDimensionalInterfaceCapability> lazyItemHandler = LazyOptional.of(() -> this.itemHandler);
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
 
+public class InterDimensionalInterfaceBlockEntity extends AEBaseInvBlockEntity {
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public InterDimensionalInterfaceBlockEntity(BlockPos pos, BlockState state) {
-        super(BlockEntityTypeInit.INTER_DIMENSIONAL_INTERFACE_ENTITY_TYPE.get(), pos, state);
+    private final InternalInventory internalInventory = new TestInternalInventory();
+
+    public InterDimensionalInterfaceBlockEntity(BlockPos pos, BlockState blockState) {
+        super(BlockEntityTypeInit.TEST_ENTITY_TYPE.get(), pos, blockState);
     }
 
     @Override
-    public void load(CompoundTag nbt) {
-        itemHandler.deserializeNBT(nbt.getCompound("inventory"));
-        super.load(nbt);
+    public InternalInventory getInternalInventory() {
+        return internalInventory;
     }
 
     @Override
-    public void saveAdditional(CompoundTag nbt) {
-        nbt.put("inventory", itemHandler.serializeNBT());
-        super.saveAdditional(nbt);
+    public void onChangeInventory(InternalInventory inv, int slot) {
+        LOGGER.info("onChangeInventory " + slot + " " + inv.toString());
     }
 
     @Override
-    public <T> @NotNull LazyOptional<T> getCapability(@NotNull Capability<T> capability, Direction direction) {
-        if (capability == ForgeCapabilities.ITEM_HANDLER) {
-            return this.lazyItemHandler.cast();
+    public void saveAdditional(CompoundTag data) {
+        // Disabled due to LAG when containing large amount of different items
+    }
+
+    @Override
+    public void loadTag(CompoundTag data) {
+        // Disabled due to LAG when containing large amount of different items
+    }
+
+    public static class TestInternalInventory implements InternalInventory {
+        HashMap<Integer, ItemStack> hash = new HashMap<>();
+
+        @Override
+        public int size() {
+            LOGGER.info("Inv Size: " + hash.size() + 1);
+            return hash.size() + 1;
         }
-        return super.getCapability(capability, direction);
+
+        @Override
+        public ItemStack getStackInSlot(int slotIndex) {
+            ItemStack itemStack = hash.get(slotIndex);
+            if(itemStack == null) return ItemStack.EMPTY;
+            return itemStack;
+        }
+
+        @Override
+        public void setItemDirect(int slotIndex, ItemStack stack) {
+            hash.put(slotIndex, stack);
+        }
     }
 
-    @Override
-    public void invalidateCaps() {
-        super.invalidateCaps();
-        this.lazyItemHandler.invalidate();
+    public static class DedicatedInternalInventory extends BaseInternalInventory {
+        private JedisPoolConfig poolConfig;
+        private JedisPool jedisPool;
+        private Jedis jedis;
+
+        public DedicatedInternalInventory (String RedisHost) {
+            poolConfig = new JedisPoolConfig();
+            jedisPool = new JedisPool(poolConfig, RedisHost);
+            jedis = jedisPool.getResource();
+        }
+
+        @Override
+        public int size() {
+            return Math.toIntExact(jedis.dbSize() + 1);
+        }
+
+        @Override
+        public ItemStack getStackInSlot(int slotIndex) {
+            String data = jedis.get(calculateRedisKey(slotIndex));
+            if(data == null) return ItemStack.EMPTY;
+            CompoundTag compoundTag = deserializeCompoundTag(data);
+            return ItemStack.of(compoundTag);
+        }
+
+        @Override
+        public void setItemDirect(int slotIndex, ItemStack stack) {
+            CompoundTag compoundTag = stack.serializeNBT();
+            jedis.set(calculateRedisKey(slotIndex), serializeCompoundTag(compoundTag));
+        }
+
+
+        public static String calculateRedisKey(int slotIndex) {
+            return "0.inv/" + slotIndex + ".slot";
+        }
+
+
+        public static String serializeCompoundTag(CompoundTag what){
+            try {
+                return what.toString();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public static CompoundTag deserializeCompoundTag(String data){
+            try {
+                return TagParser.parseTag(data);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
-
-
 
 }
--- src/main/java/net/nussi/dedicated_applied_energistics/blockentities/TestBlockEntity.java
+++ src/main/java/net/nussi/dedicated_applied_energistics/blockentities/TestBlockEntity.java
@@ -1,34 +1,22 @@
 package net.nussi.dedicated_applied_energistics.blockentities;
 
-import appeng.api.config.AccessRestriction;
+import appeng.api.inventories.BaseInternalInventory;
 import appeng.api.inventories.InternalInventory;
-import appeng.api.networking.IInWorldGridNodeHost;
-import appeng.api.storage.IStorageMounts;
-import appeng.api.storage.IStorageProvider;
-import appeng.api.storage.MEStorage;
-import appeng.api.storage.StorageCells;
-import appeng.api.storage.cells.StorageCell;
-import appeng.api.util.AECableType;
-import appeng.blockentity.grid.AENetworkInvBlockEntity;
-import appeng.blockentity.inventory.AppEngCellInventory;
-import appeng.core.definitions.AEItems;
-import appeng.me.cells.BasicCellHandler;
-import appeng.me.cells.CreativeCellHandler;
-import appeng.me.storage.MEInventoryHandler;
-import appeng.me.storage.NullInventory;
-import appeng.parts.storagebus.StorageBusPart;
+import appeng.blockentity.AEBaseInvBlockEntity;
 import com.mojang.logging.LogUtils;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 import net.nussi.dedicated_applied_energistics.init.BlockEntityTypeInit;
 import org.slf4j.Logger;
 
-public class TestBlockEntity extends AENetworkInvBlockEntity implements IStorageProvider {
+import java.util.HashMap;
+
+public class TestBlockEntity extends AEBaseInvBlockEntity {
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    private AppEngCellInventory inv = new AppEngCellInventory(this, 1);
+    private final DedicatedInternalInventory internalInventory = new DedicatedInternalInventory();
 
     public TestBlockEntity(BlockPos pos, BlockState blockState) {
         super(BlockEntityTypeInit.TEST_ENTITY_TYPE.get(), pos, blockState);
@@ -36,7 +24,7 @@ public class TestBlockEntity extends AENetworkInvBlockEntity implements IStorage
 
     @Override
     public InternalInventory getInternalInventory() {
-        return inv;
+        return internalInventory;
     }
 
     @Override
@@ -45,37 +33,35 @@ public class TestBlockEntity extends AENetworkInvBlockEntity implements IStorage
     }
 
     @Override
-    public void mountInventories(IStorageMounts storageMounts) {
-
-        StorageCell storageCell = BasicCellHandler.INSTANCE.getCellInventory(AEItems.ITEM_CELL_256K.stack(), null);
-        inv.setHandler(0, storageCell);
-        StorageBusInventory handler = new StorageBusInventory(storageCell);
-        storageMounts.mount(handler);
+    public void saveAdditional(CompoundTag data) {
+        // Disabled due to LAG when containing large amount of different items
     }
 
     @Override
-    public AECableType getCableConnectionType(Direction dir) {
-        return AECableType.SMART;
+    public void loadTag(CompoundTag data) {
+        // Disabled due to LAG when containing large amount of different items
     }
 
-    static class StorageBusInventory extends MEInventoryHandler {
-        public StorageBusInventory(MEStorage inventory) {
-            super(inventory);
+    public static class DedicatedInternalInventory extends BaseInternalInventory {
+        HashMap<Integer, ItemStack> hash = new HashMap<>();
+
+        @Override
+        public int size() {
+            LOGGER.info("Inv Size: " + hash.size());
+            return hash.size() + 1;
         }
 
         @Override
-        protected MEStorage getDelegate() {
-            return super.getDelegate();
+        public ItemStack getStackInSlot(int slotIndex) {
+            ItemStack out = hash.get(slotIndex);
+            if(out == null) out = ItemStack.EMPTY;
+            return out;
         }
 
         @Override
-        protected void setDelegate(MEStorage delegate) {
-            super.setDelegate(delegate);
-        }
-
-        public void setAccessRestriction(AccessRestriction setting) {
-            setAllowExtraction(setting.isAllowExtraction());
-            setAllowInsertion(setting.isAllowInsertion());
+        public void setItemDirect(int slotIndex, ItemStack stack) {
+            hash.put(slotIndex, stack);
         }
     }
+
 }
